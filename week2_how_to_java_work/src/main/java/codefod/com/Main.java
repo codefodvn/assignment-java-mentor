78db34b08930d3d07b2b61a964469df99878ba35
unknown <minhvo.20040329@gmail.com>
2025-02-20 21:10:36 +0700

    feat: homework for w2 in codefod

--- week2_how_to_java_work/src/main/java/codefod/com/Main.java
+++ week2_how_to_java_work/src/main/java/codefod/com/Main.java
@@ -3,6 +3,14 @@ package codefod.com;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        StringBuilder string = new StringBuilder();
+        for(int i =0; i< 10;i++){
+            string.append(i+" ");
+        }
+        System.out.println(string);
     }
+
+//    java -Xms512m -Xmx1024m -Xss1m -XX:+UseG1GC -XX:+PrintGCDetails codefod.com.Main
+//    javac Main.java tao file bytecode tu class
+//    java Main.java chay chuong trinh
 }
