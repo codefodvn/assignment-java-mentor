5ce7ae97833c8e14029b728700acba8fd72a0fc7
unknown <minhvo.20040329@gmail.com>
2025-02-25 23:17:25 +0700

    java week2

--- week2_how_to_java_work/src/main/java/codefod/com/Main.java
+++ week2_how_to_java_work/src/main/java/codefod/com/Main.java
@@ -1,16 +1,21 @@
 package codefod.com;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        StringBuilder string = new StringBuilder();
-        for(int i =0; i< 10;i++){
-            string.append(i+" ");
-        }
-        System.out.println(string);
+        getNumWords("Vo quang minh");
     }
 
 //    java -Xms512m -Xmx1024m -Xss1m -XX:+UseG1GC -XX:+PrintGCDetails codefod.com.Main
 //    javac Main.java tao file bytecode tu class
 //    java Main.java chay chuong trinh
+    public static void getNumWords(String str){
+        List<String> result = Arrays.asList(str.split("\\s+"));
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
 }
